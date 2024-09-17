a60bc23f1548dfc7259fb467b4e95c7cd9a172eb
Jkdev269 <jaharuddin269@gmail.com>
2024-09-17 15:02:31 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,35 @@
+const express=require('express')
+const socketio=require('socket.io')
+const app=express()
+const path=require('path')
+app.use(express.static(path.join(__dirname,'public')))
+app.get('/',(req,res)=>{
+res.send('Testing api')
+})
+const server=app.listen(8080,()=>{console.log('server is running port 8080');
+})
+const io=socketio(server)
+io.on('connection',socket=>{
+    console.log('New connection made-',socket.id);
+    socket.on('joinRoom',({ room, userName }) =>{
+        socket.join(room);
+        socket.room = room;
+        socket.userName = userName;
+        console.log(`${userName} joined room ${room}`);
+        // console.log(`socket ${socket.id} joinRoom ${room}`);
+    }) 
+    // socket.on('message',({room,message})=>{
+    //     console.log(room,message)
+    //     socket.to(room).emit('message',{id:socket.id,message:message})
+    // })
+    socket.on('message', (data, callback) => {
+        io.to(data.room).emit('message', {
+            userName: data.userName,
+            message: data.message
+        });
+        callback('Message delivered');
+    });
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+})
